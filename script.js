ca287f0a1ca2bccc62791d9218b962fbc3453719
luiz <luizvicentemobile@gmail.com>
2025-06-13 20:38:38 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
+// Mantém apenas o essencial no front-end:
 const firebaseConfig = {
   apiKey: "AIzaSyDIfTE3Nkwb_yiDKJhmwj_o3I8guislO8g",
-  authDomain: "clinica-3f715.firebaseapp.com",
-  databaseURL: "https://clinica-3f715-default-rtdb.firebaseio.com",
-  projectId: "clinica-3f715",
-  storageBucket: "clinica-3f715.firebasestorage.app",
-  messagingSenderId: "456473690332",
-  appId: "1:456473690332:web:667b239d83c802ecb83419",
-  measurementId: "G-7XHX0XC3BD"
+  
 };
 
-
-
 firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 
 function login() {
